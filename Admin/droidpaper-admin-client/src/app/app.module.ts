5e75fcbd4918fae0c7d542caa0fa7b50d09a092e
akhilpvijayan <akhilpv88.apg@gmail.com>
2025-05-18 16:22:21 +0530

    changes

--- Admin/droidpaper-admin-client/src/app/app.module.ts
+++ Admin/droidpaper-admin-client/src/app/app.module.ts
@@ -5,7 +5,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
 import { provideStorage, getStorage } from '@angular/fire/storage';
-import { environment } from 'src/environment/environment';
 import { UploadWallpaperComponent } from './upload-wallpaper/upload-wallpaper.component';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
@@ -19,8 +18,6 @@ import { HttpClientModule } from '@angular/common/http';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    provideFirebaseApp(() => initializeApp(environment.firebase)),
-    provideStorage(() => getStorage()),
     FormsModule,
     HttpClientModule 
   ],
